a4a1765d4fbb81c6ec0e99d6f1f81d1a4c64fef7
Daniel Hines <d4hines@gmail.com>
2020-05-04 04:46:22 -0400

    refactor to use maps

--- flamingo/lib/index.js
+++ flamingo/lib/index.js
@@ -1,23 +1,23 @@
 const { Flamingo: NativeFlamingo } = require('../native');
 
 const parse_new_object = ({ type, payload }) => {
+    // The type is namespaced, e.g. "Flamingo/Move"
     // Chop off everything up to the "/"
     const sort = type.replace(/.*\//, "");
     const oid = payload.oid;
     delete payload.oid;
     const attributes = Object.keys(payload)
-        .map(k => ({
-            oid,
-            val: {
-                [k]: payload[k]
-            }
-        }));
+        .reduce((prev, curr) => {
+            // Due to the way it's serialized, every attribute
+            // gets an extra level of nesting,
+            // e.g. { height { height: 100 }}
+            prev[curr] = {[curr]: payload[curr]}
+            return prev;
+        }, {});
 
     return {
-        object: {
-            oid,
-            sort,
-        },
+        oid,
+        sort,
         attributes
     }
 }
