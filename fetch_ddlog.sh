f0302dbd9bcd670c324c778cf68f84d4302ff359
Daniel Hines <d4hines@gmail.com>
2020-05-04 07:01:30 -0400

    add local ddlog binary

---
+++ fetch_ddlog.sh
@@ -0,0 +1,4 @@
+wget -O ddlog.tar.gz https://github.com/vmware/differential-datalog/releases/download/v0.17.2/ddlog-v0.17.2-20200430164611-linux.tar.gz
+echo Unzipping DDLog... This could take a while...
+tar -xzf ddlog.tar.gz
+rm ./ddlog.tar.gz
